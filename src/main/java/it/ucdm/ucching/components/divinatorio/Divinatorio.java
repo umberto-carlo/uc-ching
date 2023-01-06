a9a01d1143066eb21144b6ab7f78d0cc1535568d
Umberto Carlo de Matteis <umberto.carlo@gmail.com>
2023-01-06 18:26:15 +0100

    Apertura files

--- src/main/java/it/ucdm/ucching/components/divinatorio/Divinatorio.java
+++ src/main/java/it/ucdm/ucching/components/divinatorio/Divinatorio.java
@@ -6,6 +6,13 @@ import it.ucdm.ucching.components.divinatorio.util.RicercaEsagrammiBuilder;
 import it.ucdm.ucching.model.Oracolo;
 import it.ucdm.ucching.util.RicercaEsagrammi;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Optional;
 
 public class Divinatorio implements IDivinatorio {
@@ -20,6 +27,19 @@ public class Divinatorio implements IDivinatorio {
         this.ricercaEsagrammi = ricercaEsagrammiBuilder.build();
     }
 
+    private void apriFile(int numeroEsagramma) {
+        try {
+            Desktop desktop = Desktop.getDesktop();
+
+            Path theFile = Files.createTempFile("ching", ".pdf");
+            InputStream is = this.getClass().getClassLoader().getResourceAsStream(String.format("pdf/i ching_compressed-%1$s.pdf", numeroEsagramma));
+            Files.copy(is, theFile, StandardCopyOption.REPLACE_EXISTING);
+            desktop.open(theFile.toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void divina() {
         System.out.println("Benvenuti ai ching");
@@ -29,5 +49,10 @@ public class Divinatorio implements IDivinatorio {
         int numeroEsagramma = this.ricercaEsagrammi.getNumero(oracolo.getEsagramma());
         Optional<Integer> numeroEsagrammaComplementare = oracolo.getEsagrammaComplementare().map(this.ricercaEsagrammi::getNumero);
         System.out.println(String.valueOf(numeroEsagramma) + numeroEsagrammaComplementare.map(String::valueOf).map(n -> " + " + n).orElse(""));
+
+        if (Desktop.isDesktopSupported()) {
+            apriFile(numeroEsagramma);
+            numeroEsagrammaComplementare.ifPresent(this::apriFile);
+        }
     }
 }
